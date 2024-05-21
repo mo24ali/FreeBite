f32f6aad8be8eaaa02a553c0a4366c38146bab99
akaGUNYY <alichere1234@gmail.com>
2024-05-21 11:46:41 +0100

    update 21/05/2024 fix AddOffreActivity sdfghjklmùfgdhfjgkhl

--- app/src/main/java/com/example/freebite2/model/OffreModel.kt
+++ app/src/main/java/com/example/freebite2/model/OffreModel.kt
@@ -1,5 +1,8 @@
 package com.example.freebite2.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class OffreModel(
     var providerID: String? = null,
     var offerID: String? = null,
@@ -9,8 +12,42 @@ data class OffreModel(
     var latitude: Double? = null,
     var longitude: Double? = null,
     var pictureUrl: String? = null,
-    var provider: User? = null,
+    var provider: User? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readDouble(),
+        parcel.readDouble(),
+        parcel.readString()
+    )
 
-) {
     constructor() : this(null, null, null, null, null, null,null, null)
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(providerID)
+        parcel.writeString(nameoffre)
+        parcel.writeString(details)
+        parcel.writeString(duration)
+        parcel.writeDouble(latitude ?: 0.0)
+        parcel.writeDouble((longitude ?: 0.0))
+        parcel.writeString(pictureUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<OffreModel> {
+        override fun createFromParcel(parcel: Parcel): OffreModel {
+            return OffreModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<OffreModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
