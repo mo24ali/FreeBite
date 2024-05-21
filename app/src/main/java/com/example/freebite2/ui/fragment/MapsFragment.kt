6cc59bd3b38857303d5b7e99e3e7b8fe4a5915cc
ahmed <ahmedtawdi00@gmail.com>
2024-05-21 13:03:40 +0100

    splash anime

--- app/src/main/java/com/example/freebite2/ui/fragment/MapsFragment.kt
+++ app/src/main/java/com/example/freebite2/ui/fragment/MapsFragment.kt
@@ -1,43 +1,6 @@
 package com.example.freebite2.ui.fragment
-/*
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Location
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
-import android.os.Bundle
-import android.os.Looper
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.google.firebase.database.*
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.example.freebite2.R
-import com.example.freebite2.model.OffreModel
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import kotlin.math.*
 
-//import android.os.Bundle
-//import com.example.freebite2.R
-import androidx.appcompat.app.AppCompatActivity
-//import com.google.android.gms.maps.CameraUpdateFactory
-//import com.google.android.gms.maps.GoogleMap
-//import com.google.android.gms.maps.OnMapReadyCallback
-//import com.google.android.gms.maps.SupportMapFragment
-//import com.google.android.gms.maps.model.LatLng
-//import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-//import com.google.android.gms.maps.model.MarkerOptions
-*/
+
 
 /*
 class MapsFragment : Fragment() {
