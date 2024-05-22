da17c952be6609c40ba4ce152ae36cddfdffd226
akaGUNYY <alichere1234@gmail.com>
2024-05-22 22:00:10 +0100

    modification recyclerVew ,add adminActivity

--- app/src/main/java/com/example/freebite2/ui/fragment/OffreDetailsFragment.kt
+++ app/src/main/java/com/example/freebite2/ui/fragment/OffreDetailsFragment.kt
@@ -1,28 +1,20 @@
 package com.example.freebite2.ui.fragment
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.freebite2.databinding.FragmentOffreDetailsBinding
+import com.example.freebite2.model.OffreModel
+import com.google.firebase.database.*
 
 class OffreDetailsFragment : Fragment() {
-    // Binding object
+
     private var _binding: FragmentOffreDetailsBinding? = null
     private val binding get() = _binding!!
-
-    // Parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,22 +24,47 @@ class OffreDetailsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Retrieve the offer ID passed from the previous fragment
+        val offerId: String? = arguments?.getString("offerId")
+
+        // Initialize Firebase database reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("offres")
+
+        // Retrieve offer details from Firebase based on the offer ID
+        offerId?.let { id ->
+            databaseReference.child(id).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val offer: OffreModel? = dataSnapshot.getValue(OffreModel::class.java)
+
+                    // Bind the offer object to the layout if it's not null
+                    offer?.let {
+                        binding.apply {
+                            this.offer = it // Set the offer object to the data variable defined in the layout
+                            executePendingBindings() // Ensure data binding is executed immediately
+                        }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Handle database error
+                    // For simplicity, you can log the error
+                    // You can also display an error message to the user
+                    databaseError.toException().printStackTrace()
+                }
+            })
+        }
+
+        // Handle back button click
+        binding.backBtn.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OffreDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ pluginManagement {
     plugins {
         id("com.github.ben-manes.versions") version "0.39.0"
         //alias(libs.plugins.jetbrainsKotlinAndroid)
+        //alias(libs.plugins.jetbrainsKotlinAndroid)
 
     }
 }
