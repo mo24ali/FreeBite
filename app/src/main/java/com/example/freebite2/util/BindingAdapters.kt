2f6f3dbc3b43a8cfb5591bc8a65ef4df0c7de34f
akaGUNYY <alichere1234@gmail.com>
2024-05-23 01:53:02 +0100

    pushihfk l'adapter

--- app/src/main/java/com/example/freebite2/util/BindingAdapters.kt
+++ app/src/main/java/com/example/freebite2/util/BindingAdapters.kt
@@ -2,20 +2,44 @@ package com.example.freebite2.util
 
 
 
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.example.freebite2.R
-import com.squareup.picasso.Picasso // Import Picasso library
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 @BindingAdapter("imageUrl")
-fun loadImage(view: ImageView, imageUrl: String?) {
-    if (!imageUrl.isNullOrEmpty()) {
-        Picasso.get() // Use Picasso.get() to obtain the Picasso instance
-            .load(imageUrl)
-            .placeholder(R.drawable.placeholder) // Set a placeholder image
-            .error(R.drawable.error_image) // Set an error image
+fun loadImage(view: ImageView, url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e("Glide", "Error loading image", e)
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
             .into(view)
-    } else {
-        view.setImageResource(R.drawable.placeholder) // Set placeholder image if URL is null or empty
     }
 }
