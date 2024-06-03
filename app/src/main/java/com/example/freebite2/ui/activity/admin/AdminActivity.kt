d79ec56275b221dcecb9cc0353c72c36e2ca609a
ahmed <ahmedtawdi00@gmail.com>
2024-06-03 08:49:58 +0100

    admin mzl

--- app/src/main/java/com/example/freebite2/ui/activity/admin/AdminActivity.kt
+++ app/src/main/java/com/example/freebite2/ui/activity/admin/AdminActivity.kt
@@ -4,11 +4,15 @@ package com.example.freebite2.ui.activity.admin
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.freebite2.adapter.OffersAdapter
 import com.example.freebite2.databinding.ActivityAdminBinding
 import com.example.freebite2.ui.activity.MainActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AdminActivity : AppCompatActivity() {
 
@@ -24,7 +28,35 @@ class AdminActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance().reference
+        // Add ValueEventListener for the "users" node
+        val usersRef = FirebaseDatabase.getInstance().getReference("Users")
+        usersRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Get the count of children under the "users" node
+                val numberOfUsers = snapshot.childrenCount.toInt()
+                // Set the text for nbUser TextView
+                binding.nbUser.text = numberOfUsers.toString()
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the error if any
+            }
+        })
+
+        // Add ValueEventListener for the "offers" node
+        val offersRef = FirebaseDatabase.getInstance().getReference("offres")
+        offersRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Get the count of children under the "offers" node
+                val numberOfOffers = snapshot.childrenCount.toInt()
+                // Set the text for nbOffre TextView
+                binding.nbOffre.text = numberOfOffers.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the error if any
+            }
+        })
         // Handle logout
         binding.logOutB.setOnClickListener {
             auth.signOut()
